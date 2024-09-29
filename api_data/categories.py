7a85d38972973f1dd826ae772740c5bbd6eb63f5
Hubert <hubertszym4@gmail.com>
2024-09-29 19:57:28 +0200

    Add languages filtering, shortened base.html code and separated api_data to a folder

---
+++ api_data/categories.py
@@ -0,0 +1,3 @@
+# These are the categories that the API supports
+categories = ["politics", "sports", "business", "technology", "entertainment", "health", "science",
+                  "lifestyle", "travel", "culture", "education", "environment", "other"]
