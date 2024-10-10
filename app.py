e3c1600c2425c9befcd7fdb55d423327c8f7e0aa
Hubert <hubertszym4@gmail.com>
2024-10-10 11:56:10 +0200

    Delete irrelevant lines of code

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-import os
 from flask import Flask
 from newslytic.routes.main import main_bp
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-
 
     # Register the blueprints
     app.register_blueprint(main_bp)
